161a867fc7d9b789e5c280db9ee2fa9af8221414
Bhaumik T <106333136+bhaumik-tripathi@users.noreply.github.com>
2022-05-28 05:04:31 +0000

    added selenium part.

--- scraper.py
+++ scraper.py
@@ -1,9 +1,29 @@
-import requests
-from bs4 import BeautifulSoup
-print('Downloaded and imported the libraries')
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 youtube_link = 'https://www.youtube.com/feed/trending'
+
+'''print('Downloaded and imported the libraries')
 response = requests.get(youtube_link)
 print('Downloading page into response object')
 print(response.text[:1000])
 
+doc = BeautifulSoup(response.text)
+print('Converted to a BeautifulSoup object !')'''
+
+def get_driver():
+  chrome_options = Options()
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  chrome_options.add_argument('--headless')
+  driver = webdriver.Chrome('/home/<user>/chromedriver',chrome_options=chrome_options)
+  return driver
+
+print('Setted a browser.')
+#if __name__ == '___main_':
+driver = get_driver()
+driver.get("https://www.python.org")
+
+print('Page title:', driver.title)
